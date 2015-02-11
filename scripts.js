36cee351e1ba73c9fb44b6594fde2dac38879660
Hampus Persson <hampus.persson@apsis.com>
2015-02-11 15:44:10 +0100

    update gulp scripts task

---
+++ scripts.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var complexity = require('gulp-complexity'),
+    concat     = require('gulp-concat'),
+    gulp       = require('gulp'),
+    jscs       = require('gulp-jscs'),
+    jshint     = require('gulp-jshint'),
+    notify     = require('gulp-notify'),
+    rename     = require('gulp-rename'),
+    size       = require('gulp-size'),
+    uglify     = require('gulp-uglify'),
+
+    moduleName = require('../package.json').name.match(/ng-(.*)/)[1];
+
+
+/**
+ * Lint all files in src/javascript using JSHint and JSCS.
+ *
+ * Config for both are found in .jshintrc and .jscsrc respectively.
+ */
+gulp.task('lint:all', ['lint:jshint', 'lint:jscs'], function gulpLint() {
+    return gulp.src([
+            'src/javascript/**/*.js'
+        ])
+        .pipe(size());
+});
+
+
+
+/**
+ * Lint all files in src/javascript using JSHint.
+ *
+ * Config for both are found in .jshintrc and .jscsrc respectively.
+ */
+gulp.task('lint:jshint', function gulpJshint() {
+    return gulp.src([
+            'modules/**/*.js'
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .on('error', notify.onError(function onError(error) {
+            return error.message;
+        }));
+});
+
+
+
+/**
+ * Lint all files in src/javascript using JSCS.
+ *
+ * Config for both are found in .jshintrc and .jscsrc respectively.
+ */
+gulp.task('lint:jscs', function gulpJscs() {
+    return gulp.src([
+            'modules/**/*.js'
+        ])
+        .pipe(jscs())
+        .on('error', notify.onError(function onError(error) {
+            return error.message;
+        }));
+});
+
+
+/**
+ * Complexity analysis of Javascript, using complexity report.
+ *
+ * Read more: https://github.com/philbooth/complexityReport.js
+ */
+gulp.task('complexity', function gulpComplexity() {
+    return gulp.src('src/javascript/**/*.js')
+        .pipe(complexity());
+});
+
+
+
+/**
+ * Build a package file of javascript in src/javascript
+ */
+gulp.task('build:js', function gulpPackageJs() {
+
+    return gulp.src([
+            'src/javascript/*.module.js',
+            'src/javascript/*.controller.js',
+            'src/javascript/*.factory.js',
+            'src/javascript/*.service.js',
+            'src/javascript/*.directive.js'
+        ])
+        .pipe(size())
+        .pipe(concat(moduleName + '.ng.js'))
+        .pipe(gulp.dest('dist/javascript'));
+});
+
+
+
+/**
+ * Build a minified version of the source.
+ *
+ * This makes a minified copy of the file that is built by task build:js.
+ */
+gulp.task('build:jsmin', ['build:js'], function gulpPackageJs() {
+    return gulp.src('dist/javascript/*.js')
+        .pipe(size())
+        .pipe(uglify())
+        .pipe(rename(function rename(path) {
+            console.log(path);
+            path.extname = '.min.js';
+        }))
+        .pipe(gulp.dest('dist/javascript'))
+        .pipe(size());
+});
+
