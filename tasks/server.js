d410d772e214427bf22f99010793fd6d29d7a24b
Hampus Persson <hampus.persson@apsis.com>
2015-02-12 10:56:54 +0100

    update browser-sync task with startPath

--- tasks/server.js
+++ tasks/server.js
@@ -5,13 +5,18 @@ var gulp = require('gulp'),
 
 gulp.task('bs', ['watch'], function() {
     browserSync({
+        server: {
+            baseDir: './'
+        },
+        startPath: 'demo/',
         files: [
             'demo/*.html',
             'demo/*.js',
             'src/javascript/*.js',
             'src/images/**/*',
             'src/templates/*.html',
-            'build/stylesheets/*.css',
-        ]
+            'build/stylesheets/*.css'
+        ],
+        browser: ['google chrome canary']
     });
 });
