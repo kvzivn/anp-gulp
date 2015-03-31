a5a62767a27b63c7f4c02cce3aa9ca73c34bc4fa
Kevin Ivan <kevin.ivan@apsis.com>
2015-03-31 13:56:24 +0200

    Change style watch from Sass to Stylus

--- src/tasks/dev.js
+++ src/tasks/dev.js
@@ -3,6 +3,6 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['clean', 'styles', 'lint:all' ], function() {
-    gulp.watch('src/stylesheets/**/*.scss', ['styles']);
+    gulp.watch('src/stylesheets/**/*.styl', ['styles']);
     gulp.watch('src/javascript/*.js', ['lint:all']);
 });
